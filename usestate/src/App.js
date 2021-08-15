9ec4c39bdf196403183cc1f8b15b34ff0007572e
theDanielTo <dto1989@gmail.com>
2021-08-15 11:54:29 -0700

    Added comment about using multiple usestate hooks

--- usestate/src/App.js
+++ usestate/src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   // const [value, setValue] = useState(initialValue)
   // if function in initialValue, put (() => function()) so it only runs once
   // if obj in initialValue, update in setValue with { ...prevValue, value: newValue }
+  //   or just use 2 setState hooks to manage 2 states (or more)
 
   function decrementCount() {
     setCount(prevCount => prevCount - 1)
