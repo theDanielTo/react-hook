2d234224f991a9185e1d374270aee5fcfdf5160a
theDanielTo <dto1989@gmail.com>
2021-08-15 10:21:36 -0700

    Completed useeffect

--- useeffect/app/src/App.js
+++ useeffect/app/src/App.js
@@ -1,23 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import FunctionContextComponent from './FunctionContextComponent';
-import ClassContextComponent from './ClassContextComponent';
+import { ThemeProvider } from './ThemeContext';
 
 export const ThemeContext = React.createContext();
 
 export default function App() {
-  const [darkThem, setDarkTheme] = useState(true);
-
-  function toggleTheme() {
-    setDarkTheme(prevDarkTheme => !prevDarkTheme);
-  }
-
   return (
-    <>
-      <ThemeContext.Provider value={darkThem}>
-        <button onClick={toggleTheme}>Toggle Theme</button>
-        <FunctionContextComponent />
-        <ClassContextComponent />
-      </ThemeContext.Provider>
-    </>
+    <ThemeProvider>
+      <FunctionContextComponent />
+    </ThemeProvider>
   );
 }
