e8c9bd3ab813362ab1874345beefa2d750eb7889
theDanielTo <dto1989@gmail.com>
2021-08-20 11:27:05 -0700

    finished exercise. read comments for notes

--- usememo/app/src/App.js
+++ usememo/app/src/App.js
@@ -1,13 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 
 export default function App() {
   const [number, setNumber] = useState(0);
   const [dark, setDark] = useState(false);
-  const doubleNumber = slowFunction(number);
-  const themeStyles = {
-    backgroundColor: dark ? 'black' : 'white',
-    color: dark ? 'white' : 'black'
-  };
+
+
+  const doubleNumber = useMemo(() => {
+    return slowFunction(number)
+  }, [number]);
+  // useMemo saves (memoize) number argument,
+  // so on re-renders, it won't be ran again on similar [number]
+
+
+  const themeStyles = useMemo(() => {
+    return {
+      backgroundColor: dark ? 'black' : 'white',
+      color: dark ? 'white' : 'black'
+    };
+  }, [dark]);
+    useEffect(() => {
+    console.log('Theme Changed')
+  }, [themeStyles])
+  // objects in JS have a specific reference (address) when created
+  // useMemo will memoize that reference.
+  // useEffect re-renders will ignore memoized objects
+
 
   return (
     <>
