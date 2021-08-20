b5364fa1d3f36b3a48cd8991ee91da5e8232b22b
theDanielTo <dto1989@gmail.com>
2021-08-20 11:59:16 -0700

    finished exercise. read comments for notes

--- useref/app/src/App.js
+++ useref/app/src/App.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function App() {
-  return (
-    <div>
+  const [name, setName] = useState('');
 
-    </div>
+
+  // const renderCount = useRef(1);
+  // returns { current: 1 }
+  // useRef kind of similar to useState,
+  // but saves the address so it stays the same through renders
+
+  // useEffect(() => {
+  //   renderCount.current = renderCount.current + 1;
+  // })
+
+
+  const inputRef = useRef();
+  function focus() {
+    // console.log(inputRef.current) // logs the element (<input value>)
+    inputRef.current.focus();
+  }
+  // ref attribute on input references inputRef (function)
+
+  const prevName = useRef('');
+  useEffect(() => {
+    prevName.current = name;
+  }, [name]);
+  // useRef used to save previous this.state
+  // persists values through renders
+
+
+  return (
+    <>
+      <input ref={inputRef} value={name} onChange={e => setName(e.target.value)}/>
+      <div>My name is {name} and it used to be {prevName.current}</div>
+      {/* <div>I rendered {renderCount.current} times</div> */}
+      {/* <button onClick={focus}>Focus</button> */}
+    </>
   );
 }
