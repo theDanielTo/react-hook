c7584b584c6afde336f8b1e64524ab542a8f9ba8
theDanielTo <dto1989@gmail.com>
2021-08-22 10:28:21 -0700

    Completed exercise

--- useeffect/app/src/App.js
+++ useeffect/app/src/App.js
@@ -1,7 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
   const [resourceType, setResourceType] = useState('posts');
+  // const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    // fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+    //   .then(res => res.json())
+    //   .then(json => setItems(json))
+    console.log('resource changed')
+
+    return () => {
+      console.log('return from resource change')
+    }
+    // clean up return code called first
+  }, [resourceType])
 
   return (
     <>
@@ -11,6 +24,31 @@ export default function App() {
         <button onClick={() => setResourceType('comments')}>Comments</button>
       </div>
       <h1>{resourceType}</h1>
+      {/* {items.map(item => (
+        <pre>{JSON.stringify(item)}</pre>
+      ))} */}
     </>
   );
+
+
+
+
+  // const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+  // const handleResize = () => {
+  //   setWindowWidth(window.innerWidth)
+  // }
+
+  // useEffect(() => {
+  //   window.addEventListener('resize', handleResize)
+  //   return () => {
+  //     window.removeEventListener('resize', handleResize)
+  //   }
+  //   // clean up
+  // }, [])
+  // // setWindowWidth changes dynamically
+
+  // return (
+  //   <div>{windowWidth}</div>
+  // )
 }
