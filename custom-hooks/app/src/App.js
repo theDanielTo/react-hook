a66ada26aa6ca560f9411ed8338105da10dc9b4a
theDanielTo <dto1989@gmail.com>
2021-08-23 22:40:17 -0700

    create dir. set up exercise

---
+++ custom-hooks/app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import useLocalStorage from './useLocalStorage'
+
+export default function App() {
+  const [name, setName] = useLocalStorage('name')
+
+  return (
+    <input type="text"
+      value={name}
+      onChange={e => setName(e.target.value)} />
+  )
+}
